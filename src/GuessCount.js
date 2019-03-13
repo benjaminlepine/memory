4390c2629912b134f6087c9df51aba08adf20bfe
Benjamin Lepine <benjamin.lepine@epitech.eu>
2019-03-13 18:04:50 +0100

    map card ok

---
+++ src/GuessCount.js
@@ -0,0 +1,9 @@
+/**
+ * Created by benjamin on 12/03/2019.
+ */
+import React from 'react'
+import './GuessCount.css'
+
+const GuessCount = ({guesses}) => <div className="guesses">{guesses}</div>
+
+export default GuessCount
